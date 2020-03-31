f93e2f00c696ae8b0744ac7d19feb174c5103c5d
liangyi <liangyi@qunhemail.com>
2020-03-31 18:51:03 +0800

    添加测试项目 spring-xiaoming-test

--- spring-xiaoming-test/src/main/java/com/xiaoming/po/Teacher.java
+++ spring-xiaoming-test/src/main/java/com/xiaoming/po/Teacher.java
@@ -1,8 +1,3 @@
-/*
- * Teacher.java
- * Copyright 2020 Qunhe Tech, all rights reserved.
- * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
- */
 
 package com.xiaoming.po;
 
--- spring-xiaoming-test/src/main/java/com/xiaoming/test/SimpleDemoTest.java
+++ spring-xiaoming-test/src/main/java/com/xiaoming/test/SimpleDemoTest.java
@@ -1,8 +1,3 @@
-/*
- * SimpleDemoTest.java
- * Copyright 2020 Qunhe Tech, all rights reserved.
- * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
- */
 
 package com.xiaoming.test;
 
